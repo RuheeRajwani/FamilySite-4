135ec3756e982244cb62d472ca632cef1fb032e4
ruhee <ruhee.rajwani@gmail.com>
2018-07-22 12:01:13 -0400

    highlight selected item on navbar

--- server.js
+++ server.js
@@ -23,64 +23,64 @@ app.set('views', path.join(__dirname, '/public/views'))
 // index page 
 
 app.get('/', function (req, res) {
-    let pageMenu = getPageMenu("index");
-    res.render('pages/index', { menu: pageMenu });
+    let selectedMenu = getSelectedMenu("home");
+    res.render('pages/', {menu: selectedMenu});;
 });
 
 
 app.get('/index', function (req, res) {
-    let pageMenu = getPageMenu("index");
-    res.render('pages/index', { menu: pageMenu });
+    let selectedMenu = getSelectedMenu("home");
+    res.render('pages/index', {menu: selectedMenu});
 
 });
 app.get('/asif', function (req, res) {
-    let pageMenu = getPageMenu("asif");
-    res.render('pages/asif', { menu: pageMenu });
+    let selectedMenu = getSelectedMenu("asif");
+    res.render('pages/asif', {menu: selectedMenu});
 });
 app.get('/contactUs', function (req, res) {
-    let pageMenu = getPageMenu("contactus");
-    res.render('pages/contactUs', { menu: pageMenu });
+    let selectedMenu = getSelectedMenu("contactUs");
+    res.render('pages/contactUs', {menu: selectedMenu});
 });
 app.get('/grandparents', function (req, res) {
-    let pageMenu = getPageMenu("grandparents");
-    res.render('pages/grandparents', { menu: pageMenu });
+    let selectedMenu = getSelectedMenu("grandparents");
+    res.render('pages/grandparents', {menu: selectedMenu});
 });
 app.get('/laila', function (req, res) {
-    let pageMenu = getPageMenu("laila");
-    res.render('pages/laila', { menu: pageMenu });
+    let selectedMenu = getSelectedMenu("laila");
+    res.render('pages/laila', {menu: selectedMenu});
 });
 app.get('/ruhee', function (req, res) {
-    let pageMenu = getPageMenu("ruhee");
-    res.render('pages/ruhee', { menu: pageMenu });
+    let selectedMenu = getSelectedMenu("ruhee");
+    res.render('pages/ruhee', {menu: selectedMenu});
 });
 app.get('/vacation', function (req, res) {
-    let pageMenu = getPageMenu("vacation");
-    res.render('pages/vacation', { menu: pageMenu });
+    let selectedMenu = getSelectedMenu("vacation");
+    res.render('pages/vacation', {menu: selectedMenu});
 });
 
 app.get('/orlando', function (req, res) {
-    let pageMenu = getPageMenu("vacation");
-    res.render('pages/orlando', { menu: pageMenu });
+    let selectedMenu = getSelectedMenu("vacation");
+    res.render('pages/orlando', {menu: selectedMenu});
 });
 app.get('/cancun', function (req, res) {
-    let pageMenu = getPageMenu("vacation");
-    res.render('pages/cancun', { menu: pageMenu });
+    let selectedMenu = getSelectedMenu("vacation");
+    res.render('pages/cancun', {menu: selectedMenu});
 });
 app.get('/hawaii', function (req, res) {
-    let pageMenu = getPageMenu("vacation");
-    res.render('pages/hawaii', { menu: pageMenu });
+    let selectedMenu = getSelectedMenu("vacation");
+    res.render('pages/hawaii', {menu: selectedMenu});
 });
 app.get('/italy', function (req, res) {
-    let pageMenu = getPageMenu("vacation");
-    res.render('pages/italy', { menu: pageMenu });
+    let selectedMenu = getSelectedMenu("vacation");
+    res.render('pages/italy', {menu: selectedMenu});;
 });
 app.get('/karachi', function (req, res) {
-    let pageMenu = getPageMenu("vacation");
-    res.render('pages/karachi', { menu: pageMenu });
+    let selectedMenu = getSelectedMenu("vacation");
+    res.render('pages/karachi', {menu: selectedMenu});;
 });
 app.get('/maine', function (req, res) {
-    let pageMenu = getPageMenu("vacation");
-    res.render('pages/maine', { menu: pageMenu });
+    let selectedMenu = getSelectedMenu("vacation");
+    res.render('pages/maine', {menu: selectedMenu});;
 });
 
 
@@ -91,49 +91,30 @@ app.post('/contactUsSubmit', function (req, res) {
         familyMember: req.body.familyMember,
         message: req.body.message
     }
-    let pageMenu = getPageMenu("contactus")
+    let selectedMenu = getSelectedMenu("contactUs");
+    
     db.addContact(contact
         , (error, result) => {
             if (error) {
                 console.log('Error inserting to db ' + error);
-                res.render('pages/contactUsSubmit', { menu: pageMenu, contact: contact, isSuccess: false })
+                res.render('pages/contactUsSubmit', { menu: selectedMenu, contact: contact, isSuccess: false })
             }
             else {
-                res.render('pages/contactUsSubmit', { menu: pageMenu, contact: contact, isSuccess: true })
+                res.render('pages/contactUsSubmit', { menu: selectedMenu, contact: contact, isSuccess: true })
             }
         }
     )
 
 })
 app.get('/ahil', function (req, res) {
-    let pageMenu = getPageMenu("ahil");
-    res.render('pages/ahil', { menu: pageMenu });
+    let selectedMenu = getSelectedMenu("ahil");
+    res.render('pages/ahil', {menu: selectedMenu});
 });
 app.listen(8080);
 console.log('8080 is the magic port');
 
-const getMenu = () => {
-    let menu = [
-        { id: "index", href: "index", display: "Home", isSelected: false },
-        { id: "asif", href: "asif", display: "Asif", isSelected: false },
-        { id: "laila", href: "laila", display: "Laila", isSelected: false },
-        { id: "ahil", href: "ahil", display: "Ahil", isSelected: false },
-        { id: "ruhee", href: "ruhee", display: "Ruhee", isSelected: false },
-        { id: "grandparents", href: "grandparents", display: "Grandparents", isSelected: false },
-        { id: "vacation", href: "vacation", display: "Vacation", isSelected: false },
-        { id: "contactus", href: "contactUs", display: "Contact Us", isSelected: false }
-    ]
-    return menu;
+const getSelectedMenu = (page) => {
+    return { selectedMenu: page };
 }
 
-const getPageMenu = (page) => {
-    let menu = getMenu();
-    var pageMenu = menu.map(function (menuItem, index, array) {
-        if (menuItem.id == page)
-            menuItem.isSelected = true;
-        return menuItem;
-    });
-
-    return pageMenu;
-}
 
